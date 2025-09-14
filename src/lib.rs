6c830fc5c7ca4f9de7a354c506f0c6f73dcc9623
Evie <14899090+evie-calico@users.noreply.github.com>
2025-09-14 17:48:09 -0400

    Add dst slice destructurer (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -44,7 +44,12 @@
 //!
 //! // allocate another user, this time using an iterator
 //! let v = vec![0, 1, 2, 3, 4];
-//! let c = User::build(33, v);
+//! let c = User::build(33, v.iter().copied());
+//!
+//! // destructure this user and compare its key to the vector
+//! // this has the advantage of iterating over u8, not &u8 or &mut u8.
+//! let (_age, signing_key) = User::destructure(c);
+//! assert!(signing_key.eq(v.into_iter()));
 //! ```
 //! Here's another example, this time using a string as the last field of a struct:
 //!
@@ -69,23 +74,23 @@
 //!
 //! // a trait we'll use in our DST
 //! trait NumberProducer {
-//!    fn get_number(&self) -> u32;
+//!     fn get_number(&self) -> u32;
 //! }
 //!
 //! // an implementation of the trait we're going to use
 //! struct FortyTwoProducer;
 //! impl NumberProducer for FortyTwoProducer {
-//!    fn get_number(&self) -> u32 {
-//!        42
-//!    }
+//!     fn get_number(&self) -> u32 {
+//!         42
+//!     }
 //! }
 //!
 //! // another implementation of the trait we're going to use
 //! struct TenProducer;
 //! impl NumberProducer for TenProducer {
-//!    fn get_number(&self) -> u32 {
-//!        10
-//!    }
+//!     fn get_number(&self) -> u32 {
+//!         10
+//!     }
 //! }
 //!
 //! #[make_dst_factory]
@@ -110,8 +115,8 @@
 //! # Attribute Features
 //!
 //! The common use case for the #[[`macro@make_dst_factory`]] attribute is to not pass any arguments.
-//! This results in factory functions called `build` when using a string or dynamic trait as the
-//! last field of the struct, and `build` and `build_from_slice` when using an array as the last
+//! This results in a function called `build` when using a string or dynamic trait as the
+//! last field of the struct, and the functions `build`, `build_from_slice`, and `destructure` when using an array as the last
 //! field of the struct.
 //!
 //! The generated functions are private by default and have the following signatures:
@@ -127,6 +132,8 @@
 //! where
 //!     last_field_type: Copy + Sized;
 //!
+//! fn destructure(this: Box<Self>) -> (Type1, Type2, ..., SelfIter);
+//!
 //! // for strings
 //! fn build(field1, field2, ..., last_field: impl AsRef<str>) -> Box<Self>;
 //!
@@ -141,22 +148,28 @@
 //! grammar is:
 //!
 //! ```ignore
-//! #[make_dst_factory(<base_factory_name>[, <visibility>] [, no_std] [, generic=<generic_name>])]
+//! #[make_dst_factory(<base_factory_name> [, destructurer=<destructurer_name>] [, iterator=<iterator_name>] [, <visibility>] [, no_std] [, generic=<generic_name>])]
 //! ```
 //!
 //! Some examples:
 //!
 //! ```ignore
-//! // The factory functions will be private and called `create` and `create_from_slice`
+//! // The generated functions will be public and called `build`, `build_from_slice`, and `destructure`.
+//! #[make_dst_factory(pub)]
+//!
+//! // The generated functions will be private and called `create`, `create_from_slice`, and `destructure`.
 //! #[make_dst_factory(create)]
 //!
-//! // The factory functions will be public and called `create` and `create_from_slice`
+//! // The generated functions will be private and called `create`, `create_from_slice`, and `destroy`.
+//! #[make_dst_factory(create, destructurer = destroy)]
+//!
+//! // The generated functions will be public and called `create`, `create_from_slice`, and `destructure`
 //! #[make_dst_factory(create, pub)]
 //!
-//! // The factory functions will be private, called `create` and `create_from_slice`, and support the `no_std` environment
+//! // The generated functions will be private, called `create`, `create_from_slice`, and `destructure`, and support the `no_std` environment
 //! #[make_dst_factory(create, no_std)]
 //!
-//! // The factory functions will be private, called `create` and `create_from_slice`,
+//! // The generated functions will be private, called `create`, `create_from_slice`, and `destructure`,
 //! // support the `no_std` environment, and will have generic types called `X`.
 //! #[make_dst_factory(create, no_std, generic=X)]
 //! ```
@@ -221,6 +234,7 @@ struct StructInfo<'a> {
     header_fields: Box<[&'a Field]>,
     header_field_idents: Box<[FieldIdent]>,
     header_param_idents: Box<[Ident]>,
+    header_types: Vec<Type>,
     tail_field: &'a Field,
     tail_field_ident: FieldIdent,
     tail_param_ident: Ident,
@@ -303,6 +317,8 @@ impl<'a> StructInfo<'a> {
             })
             .collect();
 
+        let header_types: Vec<_> = header_fields.iter().map(|field| field.ty.clone()).collect();
+
         let tail_param_ident = tail_field
             .ident
             .as_ref()
@@ -319,6 +335,7 @@ impl<'a> StructInfo<'a> {
             header_fields: header_fields.into_boxed_slice(),
             header_field_idents: header_field_idents.into_boxed_slice(),
             header_param_idents: header_param_idents.into_boxed_slice(),
+            header_types,
             tail_field,
             tail_field_ident,
             tail_param_ident,
@@ -372,12 +389,24 @@ fn tail_layout<T: quote::ToTokens>(tail_type: &T, span: Span) -> TokenStream {
     quote_spanned! { span => ::core::alloc::Layout::array::<#tail_type>(len).expect("Array exceeds maximum size allowed of isize::MAX") }
 }
 
-fn guard_type(macro_args: &MacroArgs) -> TokenStream {
-    let dealloc_path = if macro_args.no_std {
+fn dealloc_path(no_std: bool) -> TokenStream {
+    if no_std {
         quote! { ::alloc::alloc::dealloc }
     } else {
         quote! { ::std::alloc::dealloc }
-    };
+    }
+}
+
+fn box_path(no_std: bool) -> TokenStream {
+    if no_std {
+        quote! { ::alloc::boxed::Box }
+    } else {
+        quote! { ::std::boxed::Box }
+    }
+}
+
+fn guard_type(macro_args: &MacroArgs) -> TokenStream {
+    let dealloc_path = dealloc_path(macro_args.no_std);
 
     quote! {
         struct Guard<T> {
@@ -433,29 +462,19 @@ fn args_tuple_assignment(struct_info: &StructInfo) -> TokenStream {
     }
 }
 
-fn alloc_funcs(no_std: bool) -> (TokenStream, TokenStream) {
-    let (box_path, alloc_path, handle_alloc_error) = if no_std {
-        (
-            quote! { ::alloc::boxed::Box },
-            quote! { ::alloc::alloc::alloc },
-            quote! { panic!("out of memory") },
-        )
+fn alloc(no_std: bool) -> TokenStream {
+    let (alloc_path, handle_alloc_error) = if no_std {
+        (quote! { ::alloc::alloc::alloc }, quote! { panic!("out of memory") })
     } else {
-        (
-            quote! { ::std::boxed::Box },
-            quote! { ::std::alloc::alloc },
-            quote! { ::std::alloc::handle_alloc_error(layout) },
-        )
+        (quote! { ::std::alloc::alloc }, quote! { ::std::alloc::handle_alloc_error(layout) })
     };
 
-    let alloc = quote! {
+    quote! {
         let mem_ptr = #alloc_path(layout);
         if mem_ptr.is_null() {
             #handle_alloc_error
         }
-    };
-
-    (alloc, box_path)
+    }
 }
 
 fn alloc_zst(box_path: &TokenStream, for_trait: bool) -> TokenStream {
@@ -491,7 +510,8 @@ fn factory_for_slice_arg(macro_args: &MacroArgs, struct_info: &StructInfo, tail_
 
     factory_where_clause.predicates.push(copy_bound_tokens);
 
-    let (alloc, box_path) = alloc_funcs(macro_args.no_std);
+    let alloc = alloc(macro_args.no_std);
+    let box_path = box_path(macro_args.no_std);
     let make_zst = alloc_zst(&box_path, false);
 
     let tail_layout = tail_layout(tail_elem_type, struct_info.tail_field.ty.span());
@@ -552,7 +572,8 @@ fn factory_for_slice_arg(macro_args: &MacroArgs, struct_info: &StructInfo, tail_
 
 fn factory_for_iter_arg(macro_args: &MacroArgs, struct_info: &StructInfo, tail_type: &Type) -> TokenStream {
     let guard_type_tokens = guard_type(macro_args);
-    let (alloc, box_path) = alloc_funcs(macro_args.no_std);
+    let alloc = alloc(macro_args.no_std);
+    let box_path = box_path(macro_args.no_std);
     let make_zst = alloc_zst(&box_path, false);
 
     let tail_layout = tail_layout(tail_type, struct_info.tail_field.ty.span());
@@ -631,8 +652,102 @@ fn factory_for_iter_arg(macro_args: &MacroArgs, struct_info: &StructInfo, tail_t
     }
 }
 
+fn destructurer_iterator_type(macro_args: &MacroArgs, struct_info: &StructInfo, tail_type: &Type) -> TokenStream {
+    let dealloc_path = dealloc_path(macro_args.no_std);
+
+    let visibility = &macro_args.visibility;
+    let iterator_name = macro_args
+        .iterator_name
+        .clone()
+        .unwrap_or_else(|| Ident::new(&format!("{}Iter", struct_info.struct_name), proc_macro2::Span::call_site()));
+
+    let struct_name = &struct_info.struct_name;
+    let struct_generics = &struct_info.struct_generics;
+    let (impl_generics, ty_generics, where_clause) = struct_info.struct_generics.split_for_impl();
+
+    let factory_doc = format!("Iterator type for a destructured `Box<{struct_name}>`");
+
+    quote! {
+        #[doc = #factory_doc]
+        #visibility struct #iterator_name #struct_generics #where_clause {
+            ptr: *mut #tail_type,
+            index: usize,
+            len: usize,
+
+            free_ptr: *mut u8,
+            layout: ::core::alloc::Layout,
+        }
+
+        impl #impl_generics ::core::iter::Iterator for #iterator_name #ty_generics #where_clause {
+            type Item = #tail_type;
+
+            fn next(&mut self) -> Option<Self::Item> {
+                if self.index >= self.len {
+                    return None;
+                }
+                #[allow(clippy::zst_offset)]
+                let value = unsafe { self.ptr.add(self.index).read() };
+                self.index += 1;
+                Some(value)
+            }
+        }
+
+        impl #impl_generics ::core::ops::Drop for #iterator_name #ty_generics #where_clause {
+            fn drop(&mut self) {
+                unsafe { #dealloc_path(self.free_ptr, self.layout) }
+            }
+        }
+    }
+}
+
+fn destructurer_with_iter(macro_args: &MacroArgs, struct_info: &StructInfo) -> TokenStream {
+    let box_path = box_path(macro_args.no_std);
+
+    let header_fields = &struct_info.header_field_idents;
+    let header_types = &struct_info.header_types;
+
+    let visibility = &macro_args.visibility;
+    let destructurer_name = &macro_args.base_destructurer_name;
+
+    let tail_field = &struct_info.tail_field_ident;
+    let struct_name = &struct_info.struct_name;
+    let iterator_name = macro_args
+        .iterator_name
+        .clone()
+        .unwrap_or_else(|| Ident::new(&format!("{}Iter", struct_info.struct_name), proc_macro2::Span::call_site()));
+    let (_impl_generics, ty_generics, _where_clause) = struct_info.struct_generics.split_for_impl();
+
+    let factory_doc = format!("Destructures an instance of `Box<{struct_name}>`, returning the tail slice as an iterator.");
+
+    quote! {
+        #[doc = #factory_doc]
+        #visibility fn #destructurer_name(
+            this: #box_path<Self>,
+        ) -> ( #( #header_types, )* #iterator_name #ty_generics)
+        {
+            let layout = ::core::alloc::Layout::for_value(&*this);
+            let len = this.#tail_field.len();
+            let this = #box_path::into_raw(this);
+            unsafe {
+                (
+                    #( (&raw mut (*this).#header_fields).read(), )*
+                    #iterator_name {
+                        ptr: (*this).#tail_field.as_mut_ptr(),
+                        index: 0,
+                        len,
+
+                        free_ptr: this.cast(),
+                        layout,
+                    }
+                )
+            }
+        }
+    }
+}
+
 fn factory_for_str_arg(macro_args: &MacroArgs, struct_info: &StructInfo) -> TokenStream {
-    let (alloc, box_path) = alloc_funcs(macro_args.no_std);
+    let alloc = alloc(macro_args.no_std);
+    let box_path = box_path(macro_args.no_std);
     let make_zst = alloc_zst(&box_path, false);
 
     let tail_layout = tail_layout(&quote! { u8 }, struct_info.tail_field.ty.span());
@@ -694,7 +809,8 @@ fn factory_for_str_arg(macro_args: &MacroArgs, struct_info: &StructInfo) -> Toke
 }
 
 fn factory_for_trait_arg(macro_args: &MacroArgs, struct_info: &StructInfo, trait_path: &Path) -> TokenStream {
-    let (alloc, box_path) = alloc_funcs(macro_args.no_std);
+    let alloc = alloc(macro_args.no_std);
+    let box_path = box_path(macro_args.no_std);
     let make_zst = alloc_zst(&box_path, true);
 
     let header_layout = header_layout(macro_args, struct_info, true);
@@ -762,10 +878,13 @@ fn make_dst_factory_impl(attr_args: TokenStream, item: TokenStream) -> SynResult
     let struct_info = StructInfo::new(&input_struct)?;
 
     let mut factories = Vec::new();
+    let mut iterator_type = None;
     match &struct_info.tail_kind {
         TailKind::Slice(elem_type) => {
             factories.push(factory_for_iter_arg(&macro_args, &struct_info, elem_type));
             factories.push(factory_for_slice_arg(&macro_args, &struct_info, elem_type));
+            factories.push(destructurer_with_iter(&macro_args, &struct_info));
+            iterator_type = Some(destructurer_iterator_type(&macro_args, &struct_info, elem_type));
         }
 
         TailKind::Str => {
@@ -786,6 +905,8 @@ fn make_dst_factory_impl(attr_args: TokenStream, item: TokenStream) -> SynResult
         impl #impl_generics #struct_name_ident #ty_generics #where_clause {
             #( #factories )*
         }
+
+        #iterator_type
     })
 }
 
--- src/macro_args.rs
+++ src/macro_args.rs
@@ -6,6 +6,9 @@ use syn::{
 
 pub struct MacroArgs {
     pub base_factory_name: Ident,
+    pub base_destructurer_name: Ident,
+    /// When `None`, defaults to appending "Iter" to the type's name.
+    pub iterator_name: Option<Ident>,
     pub visibility: Visibility,
     pub no_std: bool,
     pub generic_name: Ident,
@@ -15,6 +18,8 @@ impl Default for MacroArgs {
     fn default() -> Self {
         Self {
             base_factory_name: Ident::new("build", proc_macro2::Span::call_site()),
+            base_destructurer_name: Ident::new("destructure", proc_macro2::Span::call_site()),
+            iterator_name: None,
             visibility: Visibility::Inherited,
             no_std: false,
             generic_name: Ident::new("G", proc_macro2::Span::call_site()),
@@ -30,7 +35,7 @@ impl Parse for MacroArgs {
         if input.peek(syn::Ident) {
             let ahead = input.fork();
             let ident = ahead.parse::<Ident>()?;
-            if ident != "no_std" && ident != "pub" && ident != "generic" {
+            if ident != "no_std" && ident != "pub" && ident != "generic" && ident != "destructurer" && ident != "iterator" {
                 result.base_factory_name = ident;
 
                 input.advance_to(&ahead);
@@ -38,6 +43,44 @@ impl Parse for MacroArgs {
                     _ = input
                         .parse::<Token![,]>()
                         .map_err(|_ignored| input.error("Expected comma after factory name"))?;
+                }
+            }
+        }
+
+        // Check for destructurer = <ident>
+        if input.peek(syn::Ident) {
+            let ahead = input.fork();
+            let ident = ahead.parse::<Ident>()?;
+            if ident == "destructurer" {
+                input.advance_to(&ahead);
+                _ = input.parse::<Token![=]>()?;
+                result.base_destructurer_name = input
+                    .parse::<Ident>()
+                    .map_err(|_ignored| input.error("Expected identifier after `destructurer=`"))?;
+                if !input.is_empty() {
+                    _ = input
+                        .parse::<Token![,]>()
+                        .map_err(|_ignored| input.error("Expected comma after destructurer name"))?;
+                }
+            }
+        }
+
+        // Check for iterator = <ident>
+        if input.peek(syn::Ident) {
+            let ahead = input.fork();
+            let ident = ahead.parse::<Ident>()?;
+            if ident == "iterator" {
+                input.advance_to(&ahead);
+                _ = input.parse::<Token![=]>()?;
+                result.iterator_name = Some(
+                    input
+                        .parse::<Ident>()
+                        .map_err(|_ignored| input.error("Expected identifier after `iterator=`"))?,
+                );
+                if !input.is_empty() {
+                    _ = input
+                        .parse::<Token![,]>()
+                        .map_err(|_ignored| input.error("Expected comma after iterator name"))?;
                 }
             }
         }
